132c63218f437c88d684a4c10158d3d2f2c7cb38
vark <374119839@qq.com>
2026-03-10 21:17:29 +0800

    更新 next.config.ts，设置 basePath 为 '/xiaoyaozizai'

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   // 如果 GitHub 仓库名不是 <用户名>.github.io，需要设置 basePath
   // 例如仓库名为 my-repo，则取消下面这行注释并改为对应名称：
-  // basePath: '/my-repo',
+  basePath: '/xiaoyaozizai',
   trailingSlash: true,
   images: {
     // 静态导出模式下需要禁用图片优化（或使用第三方 loader）
